6d7e1a1a7871f7b08e693af8f0486e8fc267066e
jkc7848 <jkc7848@gmail.com>
2019-02-27 03:01:03 -0500

    Added: JottoManager for AI has been created. Edited: GameManager has been updated.

--- practice/Jotto/src/main/java/springboot/service/GameManager.java
+++ practice/Jotto/src/main/java/springboot/service/GameManager.java
@@ -5,44 +5,52 @@ import springboot.model.GameRound;
 import java.util.ArrayList;
 
 public class GameManager {
-    String userWord;
-    String aiWord;
-    String userGuess;
-    String aiGuess;
-    int userGuessCount;
-    int aiGuessCount;
-    int roundNum = 0;
-    int[] userColorArray;
-    int[] aiColorArray;
-    ArrayList<GameRound> gameRoundList = new ArrayList<GameRound>();
+    private String userWord;
+    private String aiWord;
+    private String userGuess;
+    private String aiGuess;
+    private int userGuessCount;
+    private int aiGuessCount;
+    private int roundNum = 0;
+    private int[] userColorArray;
+    private int[] aiColorArray;
+    private ArrayList<GameRound> gameRoundList = new ArrayList<GameRound>();
+    private JottoManager jottoManager = new JottoManager();
 
     public int getGuessCount(boolean flag) {
-        int guessCount = 0;
+        int guessCount;
         this.userGuessCount = 0;
         this.aiGuessCount = 0;
         this.userColorArray = new int[5];
         this.aiColorArray = new int[5];
 
         // flag 0 = user; flag 1 = ai
-        for (int i = 0; i < 5; i++) {
-            if (!flag) {
-                if (aiWord.contains(Character.toString(this.userGuess.charAt(i)))) {
-                    userColorArray[i] = 1;
+
+        if (!flag) { // For user guess
+            for (int i = 0; i < 5; i++) {
+                if (this.userGuess.contains(Character.toString(this.aiWord.charAt(i)))) {
                     this.userGuessCount++;
                 }
+                if (this.aiWord.contains(Character.toString(this.userGuess.charAt(i))))
+                    userColorArray[i] = 1;
                 else
                     userColorArray[i] = 0;
-                guessCount = this.userGuessCount;
             }
-            else {
-                if (userWord.contains(Character.toString(this.aiGuess.charAt(i)))) {
-                    aiColorArray[i] = 1;
+            guessCount = this.userGuessCount;
+        }
+        else { // For AI guess
+            this.aiGuess = jottoManager.chooseAiGuess(roundNum, userWord);
+
+            for (int i = 0; i < 5; i++) {
+                if (this.aiGuess.contains(Character.toString(this.userWord.charAt(i)))) {
                     this.aiGuessCount++;
                 }
+                if (this.userWord.contains(Character.toString(this.aiGuess.charAt(i))))
+                    aiColorArray[i] = 1;
                 else
                     aiColorArray[i] = 0;
-                guessCount = this.aiGuessCount;
             }
+            guessCount = this.aiGuessCount;
         }
 
         return guessCount;
@@ -55,6 +63,7 @@ public class GameManager {
         currentGameRound.setUserColorArray(userColorArray);
         currentGameRound.setAiColorArray(aiColorArray);
 
+        roundNum++;
         gameRoundList.add(currentGameRound);
     }
 }
