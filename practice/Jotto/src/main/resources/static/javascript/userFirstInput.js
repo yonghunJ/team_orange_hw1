e3c844d5e0ede53a8ec4b0df852e05bc57eed981
yonghjeong <yonghun.jeong@stonybrook.edu>
2019-02-28 18:14:58 -0500

    ajax request - user input for ai answer

--- practice/Jotto/src/main/resources/static/javascript/userFirstInput.js
+++ practice/Jotto/src/main/resources/static/javascript/userFirstInput.js
@@ -5,8 +5,30 @@ $(document).ready(function() {
         if(input.length != 5){
             console.log("fail");
         }else{
-            $('#ai_answer').val("");
-            $("#ai_answer_inadquate").text("wrong input");
+
+            //input valid check //duplication //only char
+
+            var user_first_input = $("#ai_answer").val();
+            $.ajax({
+                type:"get",
+                dataType: "json",
+                url: "http://orangeJotto.com/user_first_input?"+user_first_input,
+                success: function(data) {
+                    if(data==0){
+                        console.log("It works");
+                    }else if(data==1){
+                        console.log("Input word is not adequate");
+                        $('#ai_answer').val("");
+                        $("#ai_answer_inadquate").text("wrong input");
+                    }
+                },
+                timeout: 2000
+            }).fail(function() {
+                console.log('Fail!');
+            });
         }
     });
+
+
+
 });
