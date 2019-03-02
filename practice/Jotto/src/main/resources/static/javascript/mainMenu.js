d0c009de1dc08a1232d0f44a7b72b30949822fb7
yonghjeong <yonghun.jeong@stonybrook.edu>
2019-03-02 02:25:12 -0500

    ajax for past result

--- practice/Jotto/src/main/resources/static/javascript/mainMenu.js
+++ practice/Jotto/src/main/resources/static/javascript/mainMenu.js
@@ -29,8 +29,6 @@ $(document).ready(function() {
             dataType: "json",
             url: "/pastGameResult",
             success : function(data) {
-                console.log('Success!');
-
                 if(data.length<20){
                     for(let i=0;i<data.length;i++){
                         $("#pastGameResult1 > ul").append("list");
@@ -44,6 +42,96 @@ $(document).ready(function() {
                     }
                 }
 
+
+
+                let list1 = $("#pastGameResult1 > ul");
+                let list2 = $("#pastGameResult2 > ul");
+
+                for(let i=0; i<list1.length ; i++) {
+                    list1[i].addEventListener("click",function(){
+                        $.ajax({
+                            type:"get",
+                            dataType: "json",
+                            url: "/pastGameResult/data?data="+data,
+                            success : function(data) {
+
+
+                                for(let m=0;m<data.length;m++){
+                                    var res = data.ai_answer[0].split("");
+                                    let round = '<tr><td>'+'round'+'</td>';
+                                    let user_guess = '<td><span>'+res[0]+'</span><span>' +res[1]+'</span><span>' +res[2]+'</span><span>' +res[3]+'</span><span>' +res[4]+'</span></td>';
+                                    let guess_corr = '<td>'+'correct'+'</td></tr>'
+                                    let user_submit = $(round + user_guess + guess_corr);
+                                    user_submit.hide();
+                                    if($('#ai_result_table > tbody > tr:first').length == 0) {
+                                        $('#ai_result_table > tbody').append(user_submit);
+                                    }else{
+                                        $('#ai_result_table > tbody > tr:first').before(user_submit);
+                                    }
+                                }
+                                for(let m=0;m<data.length;m++){
+                                    var res = data.user_answer[0].split("");
+                                    let round = '<tr><td>'+'round'+'</td>';
+                                    let user_guess = '<td><span>'+res[0]+'</span><span>' +res[1]+'</span><span>' +res[2]+'</span><span>' +res[3]+'</span><span>' +res[4]+'</span></td>';
+                                    let guess_corr = '<td>'+'correct'+'</td></tr>'
+                                    let user_submit = $(round + user_guess + guess_corr);
+                                    user_submit.hide();
+                                    if($('#player_result_table > tbody > tr:first').length == 0) {
+                                        $('#player_result_table > tbody').append(user_submit);
+                                    }else{
+                                        $('#player_result_table > tbody > tr:first').before(user_submit);
+                                    }
+                                }
+
+                            },error : function(request,err){
+                                console.log('Fail!');
+                            }
+                        });
+                    });
+                }
+
+                for(let i=0; i<list1.length ; i++) {
+                    list2[i].addEventListener("click",function(){
+                        $.ajax({
+                            type:"get",
+                            dataType: "json",
+                            url: "/pastGameResult/data?data="+data,
+                            success : function(data) {
+
+
+                                for(let m=0;m<data.length;m++){
+                                    var res = data.ai_answer[0].split("");
+                                    let round = '<tr><td>'+'round'+'</td>';
+                                    let user_guess = '<td><span>'+res[0]+'</span><span>' +res[1]+'</span><span>' +res[2]+'</span><span>' +res[3]+'</span><span>' +res[4]+'</span></td>';
+                                    let guess_corr = '<td>'+'correct'+'</td></tr>'
+                                    let user_submit = $(round + user_guess + guess_corr);
+                                    user_submit.hide();
+                                    if($('#ai_result_table > tbody > tr:first').length == 0) {
+                                        $('#ai_result_table > tbody').append(user_submit);
+                                    }else{
+                                        $('#ai_result_table > tbody > tr:first').before(user_submit);
+                                    }
+                                }
+                                for(let m=0;m<data.length;m++){
+                                    var res = data.user_answer[0].split("");
+                                    let round = '<tr><td>'+'round'+'</td>';
+                                    let user_guess = '<td><span>'+res[0]+'</span><span>' +res[1]+'</span><span>' +res[2]+'</span><span>' +res[3]+'</span><span>' +res[4]+'</span></td>';
+                                    let guess_corr = '<td>'+'correct'+'</td></tr>'
+                                    let user_submit = $(round + user_guess + guess_corr);
+                                    user_submit.hide();
+                                    if($('#player_result_table > tbody > tr:first').length == 0) {
+                                        $('#player_result_table > tbody').append(user_submit);
+                                    }else{
+                                        $('#player_result_table > tbody > tr:first').before(user_submit);
+                                    }
+                                }
+
+                            },error : function(request,err){
+                                console.log('Fail!');
+                            }
+                        });
+                    });
+                }
 
             },error : function(request,err){
                 console.log('Fail!');
