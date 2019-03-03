7cd27d02f428c506209be15503a999d744086b92
yonghjeong <yonghun.jeong@stonybrook.edu>
2019-03-02 19:04:48 -0500

    Merge branch 'alpha' of C:\Users\jyh\Documents\sb_class\cse308\team_orange_jotto with conflicts.

--- practice/Jotto/src/main/resources/static/javascript/userFirstInput.js
+++ practice/Jotto/src/main/resources/static/javascript/userFirstInput.js
@@ -5,33 +5,35 @@ $(document).ready(function() {
         if(input.length != 5){
             console.log("fail");
         }else{
-            var pageNum = $("#flipbook").turn("page");
-            $("#flipbook").turn("page", pageNum+2);
+            let user_first_input = $("#ai_answer").val();
+            $('#ai_answer').val("");
+
+
 
 
             //input valid check //duplication //only char
 
-            var user_first_input = $("#ai_answer").val();
+
+
             $.ajax({
                 type:"get",
-                dataType: "json",
-                url: "http://orangeJotto.com/user_first_input?"+user_first_input,
-                success: function(data) {
+                url: "/user_first_input?"+user_first_input,
+                dataType:'json',
+                success : function(data) {
                     if(data==0){
                         console.log("It works");
 
                         var pageNum = $("#flipbook").turn("page");
-                        $("#flipbook").turn("page", pageNum+1);
+                        $("#flipbook").turn("page", pageNum+2);
 
                     }else if(data==1){
                         console.log("Input word is not adequate");
                         $('#ai_answer').val("");
                         $("#ai_answer_inadquate").text("wrong input");
                     }
-                },
-                timeout: 2000
-            }).fail(function() {
-                console.log('Fail!');
+                },error : function(request,err){
+                    console.log('Fail!');
+                }
             });
         }
     });
